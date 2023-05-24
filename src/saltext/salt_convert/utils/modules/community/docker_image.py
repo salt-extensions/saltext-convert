efc22d408d3229bc62e99636545f2ff3f97c89e5
Gareth J. Greenaway <gareth@saltstack.com>
2023-05-24 07:24:29 -0700

    Adding tests for Docker modules.

---
+++ src/saltext/salt_convert/utils/modules/community/docker_image.py
@@ -0,0 +1,61 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""
+Module for converting state file
+
+.. versionadded:: 0.0.1
+
+"""
+import inspect
+
+import salt.states.docker_image
+import saltext.salt_convert.utils.helpers as helpers
+import saltext.salt_convert.utils.inspect
+import saltext.salt_convert.utils.lookup as lookup_builtins
+
+
+def _setup():
+    """
+    Return the builtins this module should support",
+    """
+    return ["docker_image", "community.docker.docker_image"]
+
+
+@lookup_builtins.lookup_decorator
+@helpers.process_vars_decorator
+def process(builtin_data, task, vars_data=None):
+    """
+    Process tasks into Salt states
+    """
+    # Using 'state' when the ansible module does not use
+    # state as an arg
+    state = "false"
+    state_args = []
+    docker_image_states = {"present": "docker_image.present", "absent": "docker_image.absent"}
+    # manually add the args that are not automatically inspected further down
+    # usually due to **kwargs usage or a mismatch in name
+    match_args = {"build": "build", "name": "name", "tag": "tag"}
+
+    state = builtin_data.get("state")
+    if not state:
+        state = "present"
+
+    _, _func = docker_image_states[state].split(".")
+    salt_args = saltext.salt_convert.utils.inspect.function_args(
+        salt.states.docker_image,
+        _func,
+        builtin_data,
+    )
+
+    for _arg in salt_args:
+        if _arg in builtin_data:
+            if not [x for x in state_args if _arg in x]:
+                state_args.append({_arg: builtin_data[_arg]})
+
+    for _arg in match_args:
+        if _arg in builtin_data:
+            if not [x for x in state_args if _arg in x]:
+                state_args.append({match_args[_arg]: builtin_data[_arg]})
+
+    state_contents = {docker_image_states[state]: state_args}
+    return state_contents
---
+++ tests/unit/modules/test_docker_container.py
@@ -0,0 +1,91 @@
+import locale
+
+import pytest
+import saltext.salt_convert.runners.salt_convert as salt_convert_runner
+import yaml
+
+
+@pytest.fixture
+def configure_loader_modules(tmp_path):
+    return {
+        salt_convert_runner: {
+            "__opts__": {"file_roots": {"base": [str(tmp_path / "root")]}},
+        },
+    }
+
+
+def test_docker_container_present(tmp_path):
+    """
+    Test converting a file playbook
+    to sls file
+    """
+    playbook = tmp_path / "docker-playbook.yml"
+
+    with open(file=playbook, mode="w", encoding=locale.getpreferredencoding()) as fp_:
+        yaml.dump(
+            [
+                {
+                    "tasks": [
+                        {
+                            "name": "manage a docker container",
+                            "community.docker.docker_container": {
+                                "name": "mycontainer",
+                                "image": "ubuntu:14.04",
+                                "state": "present",
+                            },
+                        },
+                    ],
+                    "hosts": "servers",
+                    "remote_user": "root",
+                    "name": "manage docker container",
+                }
+            ],
+            fp_,
+        )
+
+    sls_file = salt_convert_runner.files(path=playbook)["Converted playbooks to sls files"][0]
+    with open(file=sls_file, encoding=locale.getpreferredencoding()) as fp_:
+        ret = yaml.safe_load(fp_)
+
+    assert ret == {
+        "manage a docker container": {
+            "docker_container.running": [{"name": "mycontainer"}, {"image": "ubuntu:14.04"}]
+        }
+    }
+
+
+def test_docker_container_absent(tmp_path):
+    """
+    Test converting a file playbook
+    to sls file
+    """
+    playbook = tmp_path / "docker-playbook.yml"
+
+    with open(file=playbook, mode="w", encoding=locale.getpreferredencoding()) as fp_:
+        yaml.dump(
+            [
+                {
+                    "tasks": [
+                        {
+                            "name": "remove a docker container",
+                            "community.docker.docker_container": {
+                                "name": "mycontainer",
+                                "state": "absent",
+                            },
+                        },
+                    ],
+                    "hosts": "servers",
+                    "remote_user": "root",
+                    "name": "manage docker container",
+                }
+            ],
+            fp_,
+        )
+
+    sls_file = salt_convert_runner.files(path=playbook)["Converted playbooks to sls files"][0]
+    with open(file=sls_file, encoding=locale.getpreferredencoding()) as fp_:
+        ret = yaml.safe_load(fp_)
+
+    assert ret == {
+        "remove a docker container": {"docker_container.absent": [{"name": "mycontainer"}]}
+    }
